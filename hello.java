e6287e27fed3a35f7d4bdb2af4453c0410483cf3
sai chaitanya <saichaitanya68329@gmail.com>
2025-04-02 12:44:59 +0530

    first1

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class hello{
 	public static void main(String[] args){
-		System.out.println("Hello India");
+		System.out.println("Hello Everyone");
+		System.out.println("This msg is from jenkins");
 	}
 }
