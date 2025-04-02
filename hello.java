8cebed634fa4322bb5f9bd7393cff842ad79e257
Sai68329 <108459841+Sai68329@users.noreply.github.com>
2025-04-02 14:43:46 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello{
 	public static void main(String[] args){
 		System.out.println("Hello Everyone");
-		System.out.println("This msg is from jenkins");
+		System.out.println("This msg is ");
 	}
 }
